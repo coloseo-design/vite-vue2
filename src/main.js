b90d7370acef4a7ecba1b1cdf4199fa4f90c790c
peng.deng <peng.deng@coloseo.cn>
2023-05-18 15:52:19 +0800

    [fix]:更新依赖

--- src/main.js
+++ src/main.js
@@ -1,28 +1,18 @@
 import Vue from 'vue';
 import "./style.css";
 import VueRouter from "vue-router";
-import Button from '@uni-vue/button';
 import Icon from '@uni-vue/icon';
 import ThemeProvider from '@uni-vue/ThemeProvider';
 import Header from '@uni-vue/header';
 import Dropdown from '@uni-vue/dropdown';
 import Menu from '@uni-vue/menu';
-import Tooltip from '@uni-vue/tooltip';
-import Badge from '@uni-vue/badge';
-import Message from '@uni-vue/message';
 
 import Layout from './components/layout/index.vue';
 import Editor from './pages/home/index.vue';
 
-import '@uni-vue/button/lib/styles/index.css';
 import '@uni-vue/header/lib/styles/index.css';
 import '@uni-vue/dropdown/lib/styles/index.css';
 import '@uni-vue/menu/lib/styles/index.css';
-import '@uni-vue/tooltip/lib/styles/index.css';
-import '@uni-vue/badge/lib/styles/index.css';
-import '@uni-vue/message/lib/styles/index.css';
-
-import 'highlight.js/styles/arduino-light.css'; // 引入内置样式
 
 import store from './store/index.js';
 
@@ -41,15 +31,10 @@ Vue.use(VueRouter);
 
 Vue.use({
   install(app) {
-    app.component(Button.name, Button);
     app.component(Icon.name, Icon);
     app.component(Header.name, Header);
     app.component(Dropdown.name, Dropdown);
     app.component(Menu.name, Menu);
-    app.component(Tooltip.name, Tooltip);
-    app.component(Badge.name, Badge);
-    // app.component(Demo.name, Demo);
-    app.component('Message', Message);
   },
 });
 
